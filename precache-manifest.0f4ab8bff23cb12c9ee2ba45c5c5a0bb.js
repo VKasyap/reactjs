6d3fbb21b174d2b1a2986f363a6faae3a7909c72
Kasyap2000 <velavarthipatikasyap@gmail.com>
2020-07-14 22:23:45 +0530

    Updates

---
+++ precache-manifest.0f4ab8bff23cb12c9ee2ba45c5c5a0bb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5396bfbee2e7faad07ae5abdab362e83",
+    "url": "/reactjs/index.html"
+  },
+  {
+    "revision": "866d1d782ba4b18891d7",
+    "url": "/reactjs/static/css/main.80f61ab1.chunk.css"
+  },
+  {
+    "revision": "8961004a6b589eb17a4c",
+    "url": "/reactjs/static/js/2.e1a0f1bd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactjs/static/js/2.e1a0f1bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "866d1d782ba4b18891d7",
+    "url": "/reactjs/static/js/main.b83a07f1.chunk.js"
+  },
+  {
+    "revision": "6c764a2234c56c8053c8",
+    "url": "/reactjs/static/js/runtime-main.267e7392.js"
+  }
+]);
